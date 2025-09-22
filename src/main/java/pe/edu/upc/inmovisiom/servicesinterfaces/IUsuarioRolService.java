703ca98edc77f71821759dde0fdfe6069fcc9345
kevin <u202318293@upc.edu.pe>
2025-09-22 18:52:38 -0500

    tablas

---
+++ src/main/java/pe/edu/upc/inmovisiom/servicesinterfaces/IUsuarioRolService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.inmovisiom.servicesinterfaces;
+
+import pe.edu.upc.inmovisiom.entities.UsuarioRol;
+
+import java.util.List;
+
+public interface IUsuarioRolService {
+    void insert(UsuarioRol usuarioRol);
+    List<UsuarioRol> list();
+    UsuarioRol listId(int id);
+    void delete(int id);
+    void update(UsuarioRol usuarioRol);
+}
