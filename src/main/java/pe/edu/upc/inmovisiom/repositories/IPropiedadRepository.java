3b51176253895da0dba3ada891608cb6be5b9c0c
MichelleAguilar <alessandraaguilarcrispin0@gmail.com>
2025-09-22 21:38:32 -0500

    tablas lista, mensaje y pago

--- src/main/java/pe/edu/upc/inmovisiom/repositories/IPropiedadRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.inmovisiom.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.inmovisiom.entities.Propiedad;
-
-public interface IPropiedadRepository extends JpaRepository<Propiedad,Integer> {
-}
