d74524ac800f76280781418cd8fc2b98a13797c2
MichelleAguilar <alessandraaguilarcrispin0@gmail.com>
2025-10-02 19:57:09 -0500

    Michelle

--- src/main/java/pe/edu/upc/inmovisiom/controllers/MensajeController.java
+++ src/main/java/pe/edu/upc/inmovisiom/controllers/MensajeController.java
@@ -5,70 +5,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.inmovisiom.dtos.RolDTO;
-import pe.edu.upc.inmovisiom.entities.Rol;
-import pe.edu.upc.inmovisiom.servicesinterfaces.IRolService;
+import pe.edu.upc.inmovisiom.dtos.MensajeDTO;
+import pe.edu.upc.inmovisiom.entities.Mensaje;
+import pe.edu.upc.inmovisiom.servicesinterfaces.IMensajeService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/roles")
-public class RolController {
+@RequestMapping("/mensajes")
+public class MensajeController {
+
     @Autowired
-    private IRolService rS;
+    private IMensajeService mS;
+
     @GetMapping
-    public List<RolDTO> listar(){
-        return rS.list().stream().map(y->{
+    public List<MensajeDTO> listar() {
+        return mS.list().stream().map(y -> {
             ModelMapper m = new ModelMapper();
-            return m.map(y,RolDTO.class);
+            return m.map(y, MensajeDTO.class);
         }).collect(Collectors.toList());
     }
+
     @PostMapping
-    public void insertar(@RequestBody RolDTO dto){
+    public void insertar(@RequestBody MensajeDTO dto) {
         ModelMapper m = new ModelMapper();
-        Rol r = m.map(dto, Rol.class);
-        rS.insert(r);
+        Mensaje mensaje = m.map(dto, Mensaje.class);
+        mS.insert(mensaje);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> listarId(@PathVariable("id") Integer id) {
-        Rol r = rS.listId(id);
-        if (r == null) {
+        Mensaje mensaje = mS.listId(id);
+        if (mensaje == null) {
             return ResponseEntity
                     .status(HttpStatus.NOT_FOUND)
                     .body("No existe un registro con el ID: " + id);
         }
         ModelMapper m = new ModelMapper();
-        RolDTO dto = m.map(r, RolDTO.class);
+        MensajeDTO dto = m.map(mensaje, MensajeDTO.class);
         return ResponseEntity.ok(dto);
     }
 
-
     @DeleteMapping("/{id}")
     public ResponseEntity<String> eliminar(@PathVariable("id") Integer id) {
-        Rol r = rS.listId(id);
-        if (r == null) {
+        Mensaje mensaje = mS.listId(id);
+        if (mensaje == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("No existe un registro con el ID: " + id);
         }
-        rS.delete(id);
+        mS.delete(id);
         return ResponseEntity.ok("Registro con ID " + id + " eliminado correctamente.");
     }
 
     @PutMapping
-    public ResponseEntity<String> modificar(@RequestBody RolDTO dto) {
+    public ResponseEntity<String> modificar(@RequestBody MensajeDTO dto) {
         ModelMapper m = new ModelMapper();
-        Rol r = m.map(dto, Rol.class);
+        Mensaje mensaje = m.map(dto, Mensaje.class);
 
-        Rol existente = rS.listId(r.getIdRol());
+        Mensaje existente = mS.listId(mensaje.getIdMensaje());
         if (existente == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("No se puede modificar. No existe un registro con el ID: " + r.getIdRol());
+                    .body("No se puede modificar. No existe un registro con el ID: " + mensaje.getIdMensaje());
         }
 
-        rS.update(r);
-        return ResponseEntity.ok("Registro con ID " + r.getIdRol() + " modificado correctamente.");
+        mS.update(mensaje);
+        return ResponseEntity.ok("Registro con ID " + mensaje.getIdMensaje() + " modificado correctamente.");
     }
 }
-
